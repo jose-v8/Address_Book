0ebcafd3a953fc94073bb1af7e03a366e77bbf09
Jose Vithayathil <Jose.Vithayathil@godigit.com>
2024-07-29 06:49:10 +0530

    added feature to delete a person from the contact

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -191,4 +191,28 @@ public class AddressBook {
 
         return -1;
     }
+
+    void deleteContact()
+    {
+        Scanner inp=new Scanner(System.in);
+        System.out.println("Enter the name of the person to delete the contact ");
+        System.out.print("Enter the first name :");
+        String firstName=inp.next();
+        System.out.print("\nEnter the last name :");
+        String lastName=inp.next();
+
+        int pos=findDetails(firstName,lastName);
+
+        if (pos==-1)
+        {
+            System.out.println("The entry with the name "+ firstName+" "+lastName+" is not found");
+
+        }
+        else {
+            System.out.println("The corresponding value is found..\nThe entries of the particular member is :");
+            System.out.println(allContacts.get(pos));
+            allContacts.remove(pos);
+            System.out.println("The person is succesfully removed from the contacts");
+        }
+        }
 }
--- src/AddressBookMain.java
+++ src/AddressBookMain.java
@@ -20,6 +20,7 @@ public class AddressBookMain {
             System.out.println("1). Add a new contact ");
             System.out.println("2). View contact");
             System.out.println("3). Edit the contact");
+            System.out.println("4). Delete the contact");
             System.out.println("0). To exit the program\n");
             System.out.print("Enter your choice :: ");
             int action= inp.nextInt();
@@ -32,6 +33,7 @@ public class AddressBookMain {
                 case 1->{System.out.println("Enter the following details ");AddressBook p=new AddressBook();p.addContact();}
                 case 2->{System.out.println("Displaying the details");AddressBook p=new AddressBook();p.displayContact();}
                 case 3->{System.out.println();AddressBook p=new AddressBook();p.editContact();}
+                case 4->{System.out.println();AddressBook p= new AddressBook();p.deleteContact();}
                 case 0->{System.out.println("Exiting the program......");repeat=false;}
             }
 
